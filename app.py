ca35e276f01ff8f507592e8e8453f75da228eb0d
Hayden Montgomery <hayden.montgomery815@gmail.com>
2024-04-03 15:12:18 -0400

    Added sessions to the survey. Added more checks to ensure to errors if user tries to skip questions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -8,26 +8,42 @@ app.config['SECRET_KEY'] ='secret'
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
-responses = []
+RESPONSE_GLOBAL = "responses"
 
 @app.route('/')
 def home_page():
     return render_template('home.html', survey = satisfaction_survey)
 
+@app.route('/reset', methods=["POST"])
+def reset_survey():
+    session[RESPONSE_GLOBAL] = []
+    return redirect('/questions/0')
+
 @app.route('/questions/<int:num>')
 def show_question(num):
+    responses = session.get(RESPONSE_GLOBAL)
+    if (responses is None):
+        return redirect('/')
+    
     if(num != len(responses)):
         flash("Attempting to access invalid question. Redirecting...")
         return redirect(f"/questions/{len(responses)}")
+    
     if(len(responses) == len(satisfaction_survey.questions)):
         return redirect("/complete")
+    
     return render_template('questions.html', question = satisfaction_survey.questions[num])
 
 @app.route('/answer', methods=["POST"])
 def store_answer():
     choice = request.form['answer']
+    responses = session[RESPONSE_GLOBAL]
     responses.append(choice);
-    return redirect(f'/questions/{len(responses)}')
+    session[RESPONSE_GLOBAL] = responses
+    if (len(responses) == len(satisfaction_survey.questions)):
+        return redirect('/complete')
+    else:
+        return redirect(f'/questions/{len(responses)}')
 
 @app.route('/complete')
 def complete():
